d84134b59d7cd6f16a1f1220bcb8f10c31cfabca
Dominik <dominik.dubiel@icloud.com>
2023-01-06 04:24:20 +0100

    Added JS and fixed CSS

---
+++ js/app.js
@@ -0,0 +1,28 @@
+//Get result element
+const resultText = document.getElementById("spanResult");
+
+//Get buttons
+const btnAC = document.getElementById("btnAC");
+const btnChange = document.getElementById("btnChange");
+const btnPercent = document.getElementById("btnPercent");
+const btnDivision = document.getElementById("btnDivision");
+
+const btnSeven = document.getElementById("btnSeven");
+const btnEight = document.getElementById("btnEight");
+const btnNine = document.getElementById("btnNine");
+const btnMultiplication = document.getElementById("btnMultiplication");
+
+const btnFour = document.getElementById("btnFour");
+const btnFive = document.getElementById("btnFive");
+const btnSix = document.getElementById("btnSix");
+const btnSubtraction = document.getElementById("btnSubtraction");
+
+const btnOne = document.getElementById("btnOne");
+const btnTwo = document.getElementById("btnTwo");
+const btnThree = document.getElementById("btnThree");
+const btnAddition = document.getElementById("btnAddition");
+
+const btnZero = document.getElementById("btnZero");
+const btnPoint = document.getElementById("btnPoint");
+const btnCalc = document.getElementById("btnCalc");
+
